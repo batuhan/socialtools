a03c62082914081f7adde1f05491012ad9ee5345
Batuhan Icoz <batuhanicoz@gmail.com>
2011-10-16 17:48:22 +0300

    added framework (without user_guide and license file) & .htaccess

--- application/config/tweet.php
+++ application/config/tweet.php
@@ -1,4 +1,4 @@
 <?php
 
-	$config['tweet_consumer_key'] = "YOUR-CONSUMER-KEY";
-	$config['tweet_consumer_secret'] = "YOUR-CONSUMER-SECRET";
+	$config['tweet_consumer_key'] = "pvOEV3Glyz9coX9EnCszEw";
+	$config['tweet_consumer_secret'] = "YgQSi6I1qAvjSyhf2qRcpm66AdtSxTAcq70T6jJcvs";
